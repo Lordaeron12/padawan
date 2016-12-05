de9999cb7c83ebb0b030e8e48d5fe94ad5cc2706
Diego CG <dcastano@apptitud.com.co>
2016-12-05 13:59:03 -0500

    Add production.py settings

--- padawan/padawan/settings/production.py
+++ padawan/padawan/settings/production.py
@@ -10,3 +10,10 @@ except ImportError:
     pass
 
 BASE_URL = 'https://padawan.herokuapp.com'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'project_title_db',
+    }
+}
